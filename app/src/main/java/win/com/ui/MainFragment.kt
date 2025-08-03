c00686bad806310904d8931fc2d4752a19d4a1d9
Dmitry <dsuschinsky@gmail.com>
2025-08-03 16:34:07 +0300

    clear project

--- app/src/main/java/win/com/ui/MainFragment.kt
+++ app/src/main/java/win/com/ui/MainFragment.kt
@@ -6,11 +6,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
-import win.com.MainActivity
 import win.com.R
-import win.com.ui.dashboard.DashboardFragment
 
 class MainFragment : Fragment() {
     private lateinit var sharedPreferences: SharedPreferences
@@ -21,30 +18,7 @@ class MainFragment : Fragment() {
     ): View? {
         sharedPreferences = requireActivity().getSharedPreferences("UserData", MODE_PRIVATE)
 
-        val isFirstLaunch = sharedPreferences.getBoolean("isFirstLaunch", true)
-        val hasStarted = sharedPreferences.getBoolean("hasStarted", false)
-
         val view = inflater.inflate(R.layout.fragment_main, container, false)
-
-        val startButton: Button = view.findViewById(R.id.startButton)
-
-        // Обработчик нажатия на кнопку
-        startButton.setOnClickListener {
-            if (isFirstLaunch) {
-                // Перенаправить на экран ввода параметров
-//                (activity as? MainActivity)?.hideBottomNav()
-                (activity as? MainActivity)?.openFragment(DashboardFragment())
-                (activity as? MainActivity)?.updateNavIcons("dashboard")
-
-                sharedPreferences.edit()
-                    .putBoolean("isFirstLaunch", false)
-                    .putBoolean("hasStarted", true)
-                    .apply()
-            } else {
-                // Если не первый запуск, но не нажал старт (на всякий случай)
-                (activity as? MainActivity)?.openDashboardFragment()  // Переход через MainActivity
-            }
-        }
 
         return view
     }
